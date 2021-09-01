a8e49e625033e1a79c07b033d15c04f771cad830
library <library@gmail.com>
2021-09-01 15:50:38 +0300

    web house 1st

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react, { component } from 'react';
+import './App.css';
+import h from './Components/h'
+
+class Garage extends react.Component {
+  render() {
+    return (
+      <div>
+      <h/>
+      </div>
+    );
+  }
+}
+export default Garage;
---
+++ src/components/Greet.js
@@ -0,0 +1,6 @@
+import react from 'react'
+
+function Greet() {
+    return <h1>hello </h1>
+}
+export default Greet
