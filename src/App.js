c884b429eb3faa25bdfcb93a37d108423987aee2
library <library@gmail.com>
2021-09-08 15:42:33 +0300

    Interface 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import './components/card';
-import './components/card.css';
+import Card from './components/Card';
+import './components/Card.css';
 import Greet from './components/Greet';
-import Head from './components/Head';
+
 
 function app() {
    
@@ -11,7 +11,7 @@ function app() {
 
       <div class='app'>
           <Greet/>
-          <Head />
+          <Card />
         </div>
       
     );
