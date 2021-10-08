21f6448d36dd6b1a75d9b22e6cdda046a0324d80
library <library@gmail.com>
2021-10-08 15:46:29 +0300

    dillinh with link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import './components/Card.css';
 import Greet from './components/Greet';
 import Footer from './components/Footer';
+import Route from "./components/Route";
+import First from "./components/First";
 
 
-function app() {
+export default () => {
    
     return (
-
-      <div class='app'>
+     
+      <div>
+        <Route path="/">
           <Greet/>
           <Card />
           <Footer />
+        </Route>
+          <Route path="First">
+            <First />
+          </Route>
       </div>
-       
-    );
+      
+    )  
+  
 }
-
-
-export default app;
